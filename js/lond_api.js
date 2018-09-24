182da4aca30e9d536d4ca03f3089f16d9ba53d21
Tenison <tenisonoseiowusu@gmail.com>
2018-09-24 07:27:30 +0100

    commit

--- js/lond_api.js
+++ js/lond_api.js
@@ -3,6 +3,8 @@ class api_class{
 		this.area = area;
 		this.place =  place;
 
+		//email: oseiowusu365@******.com //s************
+		//get your api key by creating an acount with https://openweathermap.org/
 		const request = new XMLHttpRequest();
 		const api_string = `http://api.openweathermap.org/data/2.5/forecast?id=${this.area}&APPID=fe7195f139f5057a9002de3b032afef3`;
 
